c5132103207e3a6760c3cd603c70231598d0e548
Jabrail Shahbazov <j.shahbazov@tayqatech.com>
2025-10-11 02:08:30 +0400

    added migration

--- src/Modules/Catalog/Catolog/GlobalUsings.cs
+++ src/Modules/Catalog/Catolog/GlobalUsings.cs
@@ -5,3 +5,5 @@ global using Catalog.Products.Events;
 global using System.Reflection;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Catalog.Data;
+global using Shared.Data;
