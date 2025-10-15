a4535ab6e9b6a67772976528038e711891796615
Jabrail Shahbazov <j.shahbazov@tayqatech.com>
2025-10-16 00:38:00 +0400

    added seq logging

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,4 +1,9 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, config) =>
+    config.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddCarterWIthAssemblies(typeof(CatalogModule).Assembly);
 
@@ -10,15 +15,14 @@ builder.Services.AddCatalogModule(builder.Configuration)
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 app.MapCarter();
+app.UseSerilogRequestLogging();
+app.UseExceptionHandler(options => { });
 
+//module services: catalog, basket, ordering
 app.UseCatalogModule()
    .UseBasketModule()
    .UseOrderingModule();
-
-app.UseExceptionHandler(options =>
-{
-    
-});
 
 app.Run();
