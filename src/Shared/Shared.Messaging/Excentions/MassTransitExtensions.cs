6b0eeecf913b6ba24379b6d48a1846b500f30479
Jabrail Shahbazov <j.shahbazov@tayqatech.com>
2025-10-26 01:35:44 +0400

    add Shared.Messaging project and integration events for messaging support

---
+++ src/Shared/Shared.Messaging/Excentions/MassTransitExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shared.Messaging.Excentions;
+
+public static class MassTransitExtensions
+{
+    public static IServiceCollection AddMassTransitWithAssemblies(this IServiceCollection services, params Assembly[] assemblies)
+    {
+        services.AddMassTransit(configure =>
+        {
+            configure.SetKebabCaseEndpointNameFormatter();
+
+            configure.SetInMemorySagaRepositoryProvider();
+
+            configure.AddConsumers(assemblies);
+            configure.AddSagaStateMachines(assemblies);
+            configure.AddSagas(assemblies);
+            configure.AddActivities(assemblies);
+
+            configure.UsingInMemory((context, configurator) =>
+            {
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+
+        return services;
+    }
+}
