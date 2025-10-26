9db7c841f15cb582ea92b1ec221cbe9ba5b1e73c
Jabrail Shahbazov <j.shahbazov@tayqatech.com>
2025-10-26 23:05:35 +0400

    remove TODO comment for cache removal in ProductPriceChangedIntegrationEventHandler

--- src/Modules/Basket/Basket/Basket/EventHandlers/ProductPriceChangedIntegrationEventHandler.cs
+++ src/Modules/Basket/Basket/Basket/EventHandlers/ProductPriceChangedIntegrationEventHandler.cs
@@ -12,7 +12,6 @@ public class ProductPriceChangedIntegrationEventHandler(
 {
     public async Task Consume(ConsumeContext<ProductPriceChangeIntegrationEvent> context)
     {
-        //TODO: remove cache for basket if any caching is implemented
         logger.LogInformation("Integration Event handled: {IntegrationEvent}", context.Message.GetType().Name);
 
         var result = await sender.Send(new UpdateItemPriceInBasketCommand(context.Message.ProductId, context.Message.Price));
